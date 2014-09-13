e288950a277627ddc212d33cb855d55babf935f4
PoTe <pote@tardis.com.uy>
2014-09-13 18:14:24 -0300

    CLI simplification

--- test/init_in_and_out_test.sh
+++ test/init_in_and_out_test.sh
@@ -5,25 +5,22 @@ GVP=../bin/gvp
 ## gvp init should create a .godeps/ directory.
 assert_raises "$GVP init"
 assert_raises "[ -d .godeps ]"
+## Cleanup
+rm -rf .godeps
 
-## source gvp in should change the original GOPATH and associated Env variables.
+## source gvp in should change the original GOPATH and associated Env variables
+## as well as creating a .godeps directory 
 ORIGINAL_GOPATH=$GOPATH
 ORIGINAL_GOBIN=$GOBIN
 ORIGINAL_PATH=$PATH
 
 source $GVP in
 
+assert_raises "[ -d .godeps ]"
 assert "echo $GOPATH" "$PWD/.godeps:$PWD"
 assert "echo $GOBIN"  "$PWD/.godeps/bin"
 assert "echo $PATH"   "$PWD/.godeps/bin:$ORIGINAL_PATH"
 
-
-### source gvp out should return env variables back to original values.
-source $GVP out
-
-assert "echo $GOPATH" "$ORIGINAL_GOPATH"
-assert "echo $GOBIN"  "$ORIGINAL_GOBIN"
-assert "echo $PATH"   "$ORIGINAL_PATH"
 
 ## Cleanup
 rm -rf .godeps
